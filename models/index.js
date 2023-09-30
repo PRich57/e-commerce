3c1162c623a649e56db8f9db11b7f336967f44e2
PRich57 <pcrichards57@gmail.com>
2023-09-30 02:39:33 -0600

    Create model associations in index

--- models/index.js
+++ models/index.js
@@ -6,14 +6,23 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: ""
-})
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+});
 
 module.exports = {
   Product,
